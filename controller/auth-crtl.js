0d80b1f56edf15ca2af46b172230f4809ad86487
PoojanBhalodiya <poojanbhalodiya02@gmail.com>
2024-03-18 00:54:19 +0530

    login

--- controller/auth-crtl.js
+++ controller/auth-crtl.js
@@ -1,7 +1,9 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../modal/usermodal.js");
 const { isValidEmail, isValidPassword } = require("../utils/validation.js");
 
-exports.createUser = async (req, res) => {
+const createUser = async (req, res) => {
   const { fullName, email, mobileNo, password, role, avatar } = req.body;
 
   // Validations
@@ -50,11 +52,12 @@ exports.createUser = async (req, res) => {
     }
 
     // Create user
+    const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({
       fullName,
       email,
       mobileNo,
-      password,
+      password: hashedPassword,
       role,
       avatar,
     });
@@ -73,4 +76,71 @@ exports.createUser = async (req, res) => {
     console.error("Error creating user: " + err.stack);
     res.status(500).send("Internal Server Error");
   }
+};
+
+const userLogin = async (req, res) => {
+  const { email, password } = req.body;
+
+  // Validation
+  if (!email || !password) {
+    return res.status(400).json({
+      statusCode: 1,
+      response: {
+        status: false,
+        message: "Email or password is missing",
+      },
+    });
+  }
+
+  try {
+    // Check if email matches
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({
+        statusCode: 1,
+        response: {
+          status: false,
+          message: "Invalid email",
+        },
+      });
+    }
+
+    // Check if password matches
+    // const passwordMatch = await bcrypt.compare(password, user.password);
+    if (password !== user.password) {
+      return res.status(400).json({
+        statusCode: 1,
+        response: {
+          status: false,
+          message: "Invalid password",
+        },
+      });
+    }
+
+    // If email and password both match
+    // Generate JWT token with email as payload
+    const token = jwt.sign(
+      { user_id: user._id, email: user.email },
+      process.env.JWT_SECRET || "fallback_secret",
+      { expiresIn: "8h" }
+    );
+
+    res.status(200).json({
+      statusCode: 1,
+      response: {
+        status: true,
+        message: "User login successful",
+        user: { id: user._id, email: user.email },
+        token,
+      },
+    });
+  } catch (err) {
+    console.error("Error logging in user: " + err.stack);
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+module.exports = {
+  createUser,
+  userLogin
 };
--- routes/auth-route.js
+++ routes/auth-route.js
@@ -4,5 +4,5 @@ const authController = require("../controller/auth-crtl");
 const router = express.Router();
 
 router.post("/register", authController.createUser);
-
+router.get("/login", authController.userLogin);
 module.exports = router;
